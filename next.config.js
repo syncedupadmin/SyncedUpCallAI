432e8118135d95ee77d02b4257232802d465996c
Nick <admin@syncedupsolutions.com>
2025-09-09 21:22:58 -0400

    fix: skip static generation for API routes

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   output: 'standalone',
   experimental: {
     serverComponentsExternalPackages: ['pg']
-  }
+  },
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true,
+  generateStaticParams: false,
+  dynamicParams: true
 }
 
 export default nextConfig
